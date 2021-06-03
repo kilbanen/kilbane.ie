0cb8e4cb99bc0e1d55c699e52f88bfe710361169
Kilbane <kilbanen@tcd.ie>
2021-06-03 20:35:06 +0100

    Changed menu bar buttons to links

--- main.js
+++
@@ -1,7 +0,0 @@
-var currentHue = 0, hueAddition = 5, nextHue, name = document.getElementById("name");
-function colourChange() {
-  var colour = "hsl(" + currentHue + ", 80%, 60%)";
-  nextHue = currentHue + hueAddition;
-  currentHue = nextHue > 360 ? 0 : nextHue;
-  document.getElementById("name").style.color = colour;
-}
